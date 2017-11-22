5b1307ac599ff9fffe9ad62c5aea8fca350f12cc
Sasha <sashamangir@mail.ru>
2017-11-22 12:09:59 +0200

    Update base.sql

--- base.sql
+++ base.sql
@@ -1,25 +1,43 @@
-/*RESET ROLE;
-DROP SCHEMA schedule1 CASCADE;*/
+DROP SCHEMA schedule1 CASCADE;
+DROP ROLE IF EXISTS  schedule_login_mangir, schedule_admin_mangir, schedule_moderator_mangir, schedule_anonim_mangir;
 
 BEGIN;
+CREATE ROLE schedule_login_mangir LOGIN PASSWORD '503fmf2017ABC';
+CREATE ROLE schedule_admin_mangir; 
+GRANT schedule_admin_mangir TO schedule_login_mangir;
+CREATE ROLE schedule_moderator_mangir;
+GRANT schedule_moderator_mangir TO schedule_login_mangir;
+CREATE ROLE schedule_anonim_mangir;
+GRANT schedule_anonim_mangir TO schedule_login_mangir;
 
 CREATE SCHEMA schedule1;
 SET search_path TO schedule1;
-
 CREATE TYPE audience AS ENUM ('lecture','practice','laboratory');
 CREATE TYPE lecture AS ENUM ('lecture','practice','laboratory');
 CREATE TYPE parity AS ENUM ('even','odd','constantly');
+CREATE TYPE education_form  AS ENUM ('full-time', 'extramural', 'part-time');
+CREATE TYPE role AS ENUM ('schedule_admin','schedule_moderator','schedule_anonim');
+CREATE TYPE jwt AS (role role, person_id int);
 
 CREATE TABLE faculty(
     id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL UNIQUE
+    name TEXT NOT NULL
 );
- 
+
 CREATE TABLE teachers(
     id SERIAL PRIMARY KEY,
-    name   TEXT NOT NULL UNIQUE,
-    science_degree TEXT NOT NULL
+    first_name   TEXT NOT NULL,
+    last_name    TEXT NOT NULL,
+    patronymic  TEXT NOT NULL,
+    science_degree TEXT NOT NULL,
+    faculty_id INTEGER REFERENCES faculty(id) NOT NULL
 );
+
+GRANT SELECT, UPDATE, DELETE, INSERT ON teachers TO schedule_admin_mangir, schedule_moderator_mangir;
+GRANT SELECT ON teachers TO schedule_anonim_mangir;
+
+GRANT SELECT, UPDATE, DELETE, INSERT ON faculty TO schedule_admin_mangir, schedule_moderator_mangir;
+GRANT SELECT ON faculty TO schedule_anonim_mangir;
 
 CREATE TABLE classroom(
     id SERIAL PRIMARY KEY,
@@ -29,21 +47,30 @@ CREATE TABLE classroom(
     type audience NOT NULL
 );
 
+GRANT SELECT, UPDATE, DELETE, INSERT ON classroom TO schedule_admin_mangir, schedule_moderator_mangir;
+GRANT SELECT ON classroom TO schedule_anonim_mangir;
+
 CREATE TABLE groups(
     id SERIAL PRIMARY KEY,
-    name VARCHAR(20) NOT NULL,
+    name   VARCHAR(20) NOT NULL,
     number_of_students Int NOT NULL,
-    faculty_id INTEGER REFERENCES faculty(id) NOT NULL
+    faculty_id INTEGER REFERENCES faculty(id) NOT NULL,
+    education_form education_form NOT NULL
+    
 );
+
+GRANT SELECT, UPDATE, DELETE, INSERT ON groups TO schedule_admin_mangir, schedule_moderator_mangir;
+GRANT SELECT ON groups TO schedule_anonim_mangir;
 
 CREATE TABLE discipline(
     id SERIAL PRIMARY KEY,
     name TEXT NOT NULL,
     group_id INTEGER REFERENCES groups(id) NOT NULL,
-    type lecture,
-    teacher_id INTEGER REFERENCES teachers(id) NOT NULL,
-    classroom_id INTEGER REFERENCES classroom(id) NOT NULL
+    type lecture
 );
+
+GRANT SELECT, UPDATE, DELETE, INSERT ON discipline TO schedule_admin_mangir, schedule_moderator_mangir;
+GRANT SELECT ON discipline TO schedule_anonim_mangir;
 
 CREATE DOMAIN day_week AS INT
 CHECK(
@@ -66,18 +93,35 @@ CREATE TABLE schedule(
     PRIMARY KEY (group_id, discipline_id,teacher_id,classroom_id,days,pair)
 );
 
+GRANT SELECT, UPDATE, DELETE, INSERT ON schedule TO schedule_admin_mangir, schedule_moderator_mangir;
+GRANT SELECT ON schedule TO schedule_anonim_mangir;
+
+
+CREATE TABLE person (
+    id SERIAL PRIMARY KEY, 
+    name VARCHAR(100)
+);
+
 CREATE TABLE account (
     id SERIAL PRIMARY KEY,
     login VARCHAR(20) NOT NULL UNIQUE,
-    password VARCHAR(20) NOT NULL
+    password VARCHAR(20) NOT NULL,
+    role role NOT NULL,
+    person_id INTEGER REFERENCES person(id),
+    faculty_id INTEGER REFERENCES faculty(id) NOT NULL
 );
 
-/*CREATE ROLE user_mangir;
-CREATE ROLE admin_mangir;*/
 
-CREATE TYPE role AS ENUM ('user_mangir', 'admin_mangir');
 
-CREATE TYPE jwt AS (role role, person_id int);
+ALTER TABLE schedule ENABLE ROW LEVEL SECURITY;
+CREATE POLICY select_schedule ON schedule FOR SELECT TO schedule_admin_mangir, schedule_moderator_mangir, schedule_anonim_mangir USING (true);
+CREATE POLICY update_schedule_admin ON schedule FOR UPDATE TO schedule_admin_mangir USING (true);
+CREATE POLICY insert_schedule_admin ON schedule FOR INSERT TO schedule_admin_mangir WITH CHECK (true);
+CREATE POLICY delete_schedule_admin ON schedule FOR DELETE TO schedule_admin_mangir USING (true);
+
+CREATE POLICY update_schedule_moderator ON schedule FOR UPDATE TO schedule_moderator_mangir USING ((SELECT faculty_id FROM account WHERE person_id=current_setting('jwt.person_id')::int)=faculty_id);
+CREATE POLICY insert_schedule_moderator ON schedule FOR INSERT TO schedule_moderator_mangir WITH CHECK ((SELECT faculty_id FROM account WHERE person_id=current_setting('jwt.person_id')::int)=faculty_id);
+CREATE POLICY delete_schedule_moderator ON schedule FOR DELETE TO schedule_moderator_mangir USING ((SELECT faculty_id FROM account WHERE person_id=current_setting('jwt.person_id')::int)=faculty_id);
 
 CREATE OR REPLACE FUNCTION change_password(password varchar(20), person_id int) RETURNS void AS 
 $$
@@ -99,32 +143,6 @@ begin
 end;
 $$ 
 LANGUAGE plpgsql SECURITY DEFINER;
-
-GRANT admin_mangir, user_mangir TO mangir;
-
-GRANT USAGE ON SCHEMA schedule1 TO admin_mangir, user_mangir;
-GRANT SELECT, UPDATE, DELETE ON discipline, teachers, classroom, groups TO admin_mangir;
-GRANT SELECT ON account TO admin_mangir;
-GRANT ALL ON schedule TO admin_mangir;
-
-ALTER TABLE discipline ENABLE ROW LEVEL SECURITY;
-ALTER TABLE teachers ENABLE ROW LEVEL SECURITY;
-ALTER TABLE classroom ENABLE ROW LEVEL SECURITY;
-ALTER TABLE groups ENABLE ROW LEVEL SECURITY;
-ALTER TABLE faculty ENABLE ROW LEVEL SECURITY;
-
-/*
-CREATE POLICY write_discipline ON discipline FOR UPDATE TO admin_mangir USING (current_setting('jwt.user_id')::int=person_id);
-CREATE POLICY delete_discipline ON discipline FOR DELETE TO admin_mangir USING (current_setting('jwt.user_id')::int=person_id);
-CREATE POLICY write_teachers ON teachers FOR DELETE TO admin_mangir USING (current_setting('jwt.user_id')::int=person_id);
-CREATE POLICY delete_teachers ON teachers FOR DELETE TO admin_mangir USING (current_setting('jwt.user_id')::int=person_id);
-CREATE POLICY write_classroom ON classroom FOR DELETE TO admin_mangir USING (current_setting('jwt.user_id')::int=person_id);
-CREATE POLICY delete_classroom ON classroom FOR DELETE TO admin_mangir USING (current_setting('jwt.user_id')::int=person_id);
-CREATE POLICY write_groups ON groups FOR DELETE TO admin_mangir USING (current_setting('jwt.user_id')::int=person_id);
-CREATE POLICY delete_groups ON groups FOR DELETE TO admin_mangir USING (current_setting('jwt.user_id')::int=person_id);
-CREATE POLICY write_faculty ON faculty FOR DELETE TO admin_mangir USING (current_setting('jwt.user_id')::int=person_id);
-CREATE POLICY delete_faculty ON faculty FOR DELETE TO admin_mangir USING (current_setting('jwt.user_id')::int=person_id);
-*/
 
 COMMIT;
 
